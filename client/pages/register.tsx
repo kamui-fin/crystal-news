3ea62ba1c73e59f8afa3e44e5d6aea1005d818ee
Kamui <fin-kamui@pm.me>
2021-07-07 18:51:34 -0500

    feat: add cors to server

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,9 +1,43 @@
-const Register = () => {
-    return (
-        <div>
-            Register
-        </div>
-    )
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+interface RegisterData {
+    username: String;
+    password: String;
+    confirmPassword: String;
+    email: String;
+};
+
+interface TokenResponse {
+    accessToken: String,
+    refreshToken: String,
 }
 
-export default Register
+const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<RegisterData>();
+    const onSubmit = async (data: RegisterData) => {
+        const res = await axios.post("http://localhost:8080/signup", data);
+        const tokenRes: TokenResponse = res.data;
+        console.log(tokenRes);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Username</label>
+            <input {...register("username")} />
+            <label>Password</label>
+            <input {...register("password")} />
+            <label>Confirm password</label>
+            <input {...register("confirmPassword")} />
+            <label>Email</label>
+            <input {...register("email")} />
+            <input type="submit" value="Sign Up" />
+        </form>
+    );
+};
+
+export default Register;
